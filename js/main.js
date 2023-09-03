ad0c5092dcb7b5491d257e9035ae01c028bb6634
Denrubkau <denrubka2@mail.ru>
2023-09-04 00:06:49 +0700

    Адаптив главной + страница О нас без адаптации

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+
     const directionsAccordion = () => {
         const accordion = document.querySelector('.directions__accordion');
 
@@ -41,10 +42,86 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    const toggleBurgerMenu = () => {
+        const openBtns = document.querySelectorAll('.burger');
+        const closeBtn = document.querySelector('.menu__close');
+        const menu = document.querySelector('.burger-menu');
+        const documentWidth = window.innerWidth;
+
+        if(documentWidth <= 767) {
+            openBtns.forEach(btn => {
+                btn.addEventListener('click', () => {
+                    menu.classList.add('open')
+                })
+            })
+
+            closeBtn.addEventListener('click', () => {
+                menu.classList.remove('open');
+            })
+
+
+        }
+    }
+
+    const trackScroll = () => {
+        const menu = document.querySelector('.burger-menu');
+        const scrolled = window.pageYOffset;
+        const coords = document.documentElement.clientHeight;
+        const pageWidth = document.documentElement.clientWidth;
+
+        if(pageWidth <= 767) {
+            if(scrolled > coords) {
+                menu.classList.add('fixed');
+            } else {
+                menu.classList.remove('fixed');
+            }
+        }
+    }
+
+
+
+    const projectsSlider = () => {
+        const projectsSwiper = new Swiper('.projects__slider', {
+            // Optional parameters
+            slidesPerView: "auto",
+
+            // Navigation arrows
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        });
+
+        if(projectsSwiper) {
+            if(window.innerWidth <= 480) {
+                projectsSwiper.destroy(true, true);
+            }
+        }
+    }
+
     const lettersSwiper = new Swiper('.letters__slider', {
         // Optional parameters
         slidesPerView: 4,
         spaceBetween: 40,
+        breakpoints: {
+            320: {
+                slidesPerView: "auto",
+                spaceBetween: 20,
+            },
+            576: {
+                slidesPerView: 2,
+            },
+            767: {
+                slidesPerView: 3,
+            },
+            1400: {
+                slidesPerView: 4,
+                spaceBetween: 27,
+            },
+            1600: {
+                spaceBetween: 40,
+            }
+        },
 
         // Navigation arrows
         navigation: {
@@ -53,18 +130,11 @@ document.addEventListener('DOMContentLoaded', () => {
         },
     });
 
-    const projectsSwiper = new Swiper('.projects__slider', {
-        // Optional parameters
-        slidesPerView: "auto",
-        spaceBetween: 80,
 
-        // Navigation arrows
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-    });
+    window.addEventListener('scroll', trackScroll)
 
+    toggleBurgerMenu();
+    projectsSlider();
     directionsAccordion();
     nextButton();
 })
